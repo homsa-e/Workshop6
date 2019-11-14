9c8bbb293312eb72d34ee89f44688bbd952ff1ad
George <piatskgeorge@mail.ru>
2019-11-14 09:35:58 +0300

    add files

---
+++ ex1/main.cpp
@@ -0,0 +1,36 @@
+/// In addition to while and for loops C++ has do .. while loop.
+/// It may be convenient eg in the following case:
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+int main (int argc, char **argv)
+{
+  for (int i = 0; i < argc; ++i)
+  {
+      std::cout << argv[i] << std::endl;
+  }
+
+
+
+  std::ifstream in ("input.csv");
+  std::string line;
+
+  while (!in.eof() && in.good ())
+  {
+    std::getline (in, line);
+    std::cout << line << std::endl;
+
+    std::vector<std::string> tokens;
+    std::stringstream ss(line);
+    std::string token;
+    while (!ss.eof () && ss.good ())
+    {
+        std::getline (ss, token, ',');
+        std::cout << token << std::endl;
+        tokens.push_back (token);
+    }
+  }
+}
---
+++ problem_1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+
+// Implement csv reader:
+
+// 1. Program should read command line arguments:
+//     input_csv file
+//     output_csv file
+//     indices of columns
+// 2. Program reads csv file, selects columns, 
+//    and outputs selected columns to output csv
+
+
+int main ()
+{
+}
+
