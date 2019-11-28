5b52235f79b3ad7bacf41ee5f1f80770f126a5ca
homsa-e <55227510+homsa-e@users.noreply.github.com>
2019-11-28 11:19:18 +0300

    Update main.cpp

--- problem_1/main.cpp
+++ problem_1/main.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
-#include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
-
-// Implement csv reader:
-
-// 1. Program should read command line arguments:
-//     input_csv file
-//     output_csv file
-//     indices of columns
-// 2. Program reads csv file, selects columns, 
-//    and outputs selected columns to output csv
-
-
-int main ()
+int main (int argc, char **argv)
 {
-}
+  for (int i = 0; i < argc; ++i)
+  {
+      //out << argv[i] << std::endl;
+  }
 
+  //return 0;
+  std::ifstream in (argv[1]);
+  std::string line;
+  std::ofstream out (argv[2]);
+  //size_t k = 0;
+  std::vector <int> columns;
+  for (int i = 3; i < argc; i++)
+      columns.push_back(atoi(argv[i]));
+  for (int i = 0; i < columns.size(); i++){
+      //out << columns[i] << std::endl;
+  }
+  while (!in.eof() && in.good ())
+  {
+    std::getline (in, line);
+    std::vector<std::string> tokens;
+    std::stringstream ss(line);
+    std::string token;
+    //out << line << std::endl;
+    while (!ss.eof () && ss.good ())
+    {
+        std::getline (ss, token, ',');
+        tokens.push_back(token);
+        //out << tokens[tokens.size() - 1] << std::endl;
+    }
+    for (int p : columns)
+        if (p != columns[columns.size() - 1])
+            out << tokens[p] << ',';
+        else
+            out << tokens[p];
+    out << '\n';
+  }
+}
